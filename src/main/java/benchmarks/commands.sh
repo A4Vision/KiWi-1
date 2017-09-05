ed9a1c18bed8eb5ebaa550b7053f45b370b47444
Niv Gabso <gabsoniv@gmail.com>
2017-09-05 06:05:42 +0300

    Write benchmarks.

---
+++ src/main/java/benchmarks/commands.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Experiment 0
+echo "Experiments Results" > results.txt
+# itemsRange=1000000 threadsAmount=* preFillAmount=500000 mapNumber=* experimentIndex=0 experimentsCount=5 testDurationSeconds=3.00 warmUpSeconds=1.00
+
+for experimentIndex in 0 1
+do
+    echo "experiment $experimentIndex"
+    echo "==========================="
+    for threadsNumber in 1 2 4 8 16 32
+    do
+        for mapNumber in 0 1 2 3
+        do
+            java -ea benchmarks.Main 1000000 $threadsNumber 500000 $mapNumber $experimentIndex 5 5 3 >> results.txt
+        done
+    done
+done
+
+for experimentIndex in 2 3
+do
+    echo "experiment $experimentIndex"
+    echo "==========================="
+    for threadsNumber in 1 2 4 8 16 32
+    do
+        for mapNumber in 0 1
+        do
+            java -ea benchmarks.Main 1000000 $threadsNumber 500000 $mapNumber $experimentIndex 5 5 3 >> results.txt
+        done
+    done
+done
