4cdc2385220ab4d57ab954962e4a02fc73832afb
Assaf <assafyifrach@mail.tau.ac.il>
2017-09-05 16:22:05 +0300

    More verbose commands script.

--- src/main/java/benchmarks/commands.sh
+++ src/main/java/benchmarks/commands.sh
@@ -2,17 +2,25 @@
 
 # Experiment 0
 echo "Experiments Results" > results.txt
-# itemsRange=2000000 threadsAmount=* preFillAmount=1000000 mapNumber=* experimentIndex=0 experimentsCount=5 testDurationSeconds=3.00 warmUpSeconds=1.00
+# Parameters order:
+# itemsRange=2000000 threadsAmount=* preFillAmount=1000000 mapNumber=* experimentIndex=5 experimentsCount=5 testDurationSeconds=3.00 warmUpSeconds=1.00
+
+export _JAVA_OPTIONS="-Xmx256m -Xms256m"
+export PREFIX="env LD_PRELOAD=libjemalloc.so numactl --interleave=all"
+export experimentsCount="5"
 
 for experimentIndex in 0 1
 do
     echo "experiment $experimentIndex"
     echo "==========================="
-    for threadsNumber in 1 2 4 8 16 32
+    for threadsNumber in 1 2 4 8 16 32 44
     do
         for mapNumber in 0 1 2 3
         do
-            java -ea benchmarks.Main 2000000 $threadsNumber 1000000 $mapNumber $experimentIndex 5 5 3 >> results.txt
+            export cmd="$PREFIX java -ea benchmarks.Main 2000000 $threadsNumber 1000000 $mapNumber $experimentIndex $experimentsCount 5 3"
+            echo $cmd
+            $cmd >> results.txt
+            tail -n 4 results.txt
         done
     done
 done
@@ -21,11 +29,14 @@ for experimentIndex in 2 3
 do
     echo "experiment $experimentIndex"
     echo "==========================="
-    for threadsNumber in 1 2 4 8 16 32
+    for threadsNumber in 1 2 4 8 16 32 44
     do
         for mapNumber in 0 1
         do
-            java -ea benchmarks.Main 1000000 $threadsNumber 500000 $mapNumber $experimentIndex 5 5 3 >> results.txt
+            export cmd="$PREFIX java -ea benchmarks.Main 2000000 $threadsNumber 1000000 $mapNumber $experimentIndex $experimentsCount 5 3"
+            echo $cmd
+            $cmd >> results.txt
+            tail -n 4 results.txt
         done
     done
 done
