4808e5ac9b401e63d4dbe34d60fcfabaedbc7276
Assaf <assafyifrach@mail.tau.ac.il>
2017-09-05 06:17:09 +0300

    Use range of 2M.

--- src/main/java/benchmarks/commands.sh
+++ src/main/java/benchmarks/commands.sh
@@ -2,7 +2,7 @@
 
 # Experiment 0
 echo "Experiments Results" > results.txt
-# itemsRange=1000000 threadsAmount=* preFillAmount=500000 mapNumber=* experimentIndex=0 experimentsCount=5 testDurationSeconds=3.00 warmUpSeconds=1.00
+# itemsRange=2000000 threadsAmount=* preFillAmount=1000000 mapNumber=* experimentIndex=0 experimentsCount=5 testDurationSeconds=3.00 warmUpSeconds=1.00
 
 for experimentIndex in 0 1
 do
@@ -12,7 +12,7 @@ do
     do
         for mapNumber in 0 1 2 3
         do
-            java -ea benchmarks.Main 1000000 $threadsNumber 500000 $mapNumber $experimentIndex 5 5 3 >> results.txt
+            java -ea benchmarks.Main 2000000 $threadsNumber 1000000 $mapNumber $experimentIndex 5 5 3 >> results.txt
         done
     done
 done
